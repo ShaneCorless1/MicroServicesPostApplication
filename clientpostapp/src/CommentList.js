f4ee7f56eded4b350a4875f53f882e58cb47bf51
Shane Corless <S00181114@mail.itsligo.ie>
2020-06-02 15:00:30 +0100

    Comment List Fixed

--- clientpostapp/src/CommentList.js
+++ clientpostapp/src/CommentList.js
@@ -1,22 +1,24 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-export default ({postId}) => {
-const [comments, setComments] = useState([]);
+export default ({ postId }) => {
+  const [comments, setComments] = useState([]);
 
-
-const fetchData = async() =>{
-    const res = await axios.get('http://localhost:4001/posts/${postId}/comments');
+  const fetchData = async () => {
+    const res = await axios.get(
+      `http://localhost:4001/posts/${postId}/comments`
+    );
 
     setComments(res.data);
-};
-useEffect(()=>{
+  };
+
+  useEffect(() => {
     fetchData();
-}, [])
-const renderedComments = comments.map(comment =>{
-    return <li key={comment.id}>{comment.content}</li>
-});
-return <ul>
-    {renderedComments}
-</ul>
-}
+  }, []);
+
+  const renderedComments = comments.map(comment => {
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
+};
