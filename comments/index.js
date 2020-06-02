a6cf8de42b37e9c6571bc664a2d4d59182d98c37
Shane Corless <S00181114@mail.itsligo.ie>
2020-06-02 19:00:25 +0100

    Parsing Events
    Parsing Incoming events with a query service to take in comments and posts

--- comments/index.js
+++ comments/index.js
@@ -1,49 +1,47 @@
 const express = require('express');
-const bodyParser = require('body-parser')
-const {
-    randomBytes
-} = require('crypto')
-const cors = require('cors')
-const axios = require('axios')
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
+const axios = require('axios');
+
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+
 const commentsByPostId = {};
 
-
 app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
 
-    res.send(commentsByPostId[req.params.id] || [])
-})
-app.post('/posts/:id/comments',async (req, res) => {
-    const commentId = randomBytes(4).toString('hex');
-    const {
-        content
-    } = req.body
+app.post('/posts/:id/comments', async (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
 
-    const comments = commentsByPostId[req.params.id] || [];
-    comments.push({
-        id: commentId,
-        content
-    });
+  const comments = commentsByPostId[req.params.id] || [];
 
-    commentsByPostId[req.params.id] = comments;
+  comments.push({ id: commentId, content });
 
-    await axios.post('localhost:4005/events',{
-        type: 'CommentCreated',
-        data: {
-            id: commentId,
-            content,
-            postId: req.params.id
+  commentsByPostId[req.params.id] = comments;
 
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id
+    }
+  });
 
-        }
-    })
-    res.status(201).send(comments)
+  res.status(201).send(comments);
+});
 
+app.post('/events', (req, res) => {
+  console.log('Event Received:', req.body.type);
 
-})
+  res.send({});
+});
 
 app.listen(4001, () => {
-    console.log('Listening on 4001');
-})
+  console.log('Listening on 4001');
+});
